b4e66aa9998d41e465cd83d8ed05aab461d01316
Joyce Yang <jcyang@hmc.edu>
2017-10-12 11:44:45 -0400

    Added loop.  Finds multiples of given modulus

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,40 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         int i = 1;
-        int n = 100;
 
-        while (i<= n){
-            if (i%3==0 && i%5!=0){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is the ending number? ");
+        int n=scan.nextInt();
+
+        System.out.println("What is the number that will be used to find multiples? ");
+         int modulus = scan.nextInt();
+         fizzbuzz(i,n);
+         System.out.println("There are "+n/7 +" multiples of 7 " + "less than or equal to "+n+ "." );
+        System.out.println("There are "+n/17 +" multiples of 17 " + "less than or equal to "+n+ "." );
+        System.out.println("There are "+n/23 +" multiples of 23 " + "less than or equal to "+n+ "." );
+        System.out.println("There are "+n/modulus +" multiples of " + modulus + " less than or equal to "+n+ "." );
+
+    }
+
+    public static void fizzbuzz(int i, int n) {
+        while (i <= n) {
+            if (i % 3 == 0 && i % 5 != 0) {
                 System.out.println("Fizz");
-            }
-            else if (i%5==0 && i%3!=0){
+            } else if (i % 5 == 0 && i % 3 != 0) {
                 System.out.println("Buzz");
-            }
-            else if (i%15 ==0){
+            } else if (i % 15 == 0) {
                 System.out.println("FizzBuzz!!");
+            } else {
+                System.out.println(i);
             }
-            else {System.out.println(i);}
             i++;
         }
-
-
     }
 }
